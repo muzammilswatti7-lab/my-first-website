c40ce3f4018d795da6dd84202a9a4d9138661493
Muzammil Swatti <muzammilswatti7@gmail.com>
2026-08-19 17:03:47 +0500

    Add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import "./globals.css";
 import AssistantLoader from "./components/AssistantLoader";
 import ContentProtection from "./components/ContentProtection";
@@ -92,15 +93,36 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-      <ContentProtection />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
-        />
-        {children} 
-        <AssistantLoader />
-      </body>
+    <body>
+  <ContentProtection />
+
+  <Script id="meta-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '1745450933368991');
+      fbq('track', 'PageView');
+    `}
+  </Script>
+
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify(organizationSchema),
+    }}
+  />
+
+  {children}
+
+  <AssistantLoader />
+</body>
     </html>
   );
 }
